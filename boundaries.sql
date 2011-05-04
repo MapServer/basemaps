178716738b36aed9d67edfab31303b5492f57cab
Thomas Bonfort <thomas.bonfort@gmail.com>
2011-05-04 17:23:43 +0000

    working script: extract linear features from shared boundaries

--- boundaries.sql
+++ boundaries.sql
@@ -7,7 +7,7 @@ create table tbt_tmp_rings (id serial, admin_level smallint, geometry geometry);
 insert into tbt_tmp_rings(admin_level,geometry) 
    SELECT admin_level, ST_Collect(ST_ExteriorRing(geometry)) AS geometry 
    FROM (
-      SELECT osm_id, admin_level, (ST_Dump(geometry)).geom As geometry
+      SELECT admin_level, osm_id, (ST_Dump(geometry)).geom As geometry
       FROM osm_new_admin) As foo
    GROUP BY admin_level,osm_id;
 
@@ -20,57 +20,57 @@ create index tbt_tmp_holes_idx on tbt_tmp_holes using gist(geometry);
 
 insert into tbt_tmp_holes(admin_level,geometry) 
 SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,1))FROM (
-      SELECT admin_level, (ST_Dump(geometry)).geom As geometry
+      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
       FROM osm_new_admin) As foo
    GROUP BY admin_level,osm_id;
 insert into tbt_tmp_holes(admin_level,geometry) 
 SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,2))FROM (
-      SELECT admin_level, (ST_Dump(geometry)).geom As geometry
+      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
       FROM osm_new_admin) As foo
    GROUP BY admin_level,osm_id;
 insert into tbt_tmp_holes(admin_level,geometry) 
 SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,3))FROM (
-      SELECT admin_level, (ST_Dump(geometry)).geom As geometry
+      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
       FROM osm_new_admin) As foo
    GROUP BY admin_level,osm_id;
 insert into tbt_tmp_holes(admin_level,geometry) 
 SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,4))FROM (
-      SELECT  admin_level, (ST_Dump(geometry)).geom As geometry
+      SELECT  admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
       FROM osm_new_admin) As foo
    GROUP BY admin_level,osm_id;
 insert into tbt_tmp_holes(admin_level,geometry) 
 SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,5))FROM (
-      SELECT admin_level, (ST_Dump(geometry)).geom As geometry
+      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
       FROM osm_new_admin) As foo
    GROUP BY admin_level,osm_id;
 insert into tbt_tmp_holes(admin_level,geometry) 
 SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,6))FROM (
-      SELECT admin_level, (ST_Dump(geometry)).geom As geometry
+      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
       FROM osm_new_admin) As foo
    GROUP BY admin_level,osm_id;
 insert into tbt_tmp_holes(admin_level,geometry) 
 SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,7))FROM (
-      SELECT admin_level, (ST_Dump(geometry)).geom As geometry
+      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
       FROM osm_new_admin) As foo
    GROUP BY admin_level,osm_id;
 insert into tbt_tmp_holes(admin_level,geometry) 
 SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,8))FROM (
-      SELECT admin_level, (ST_Dump(geometry)).geom As geometry
+      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
       FROM osm_new_admin) As foo
    GROUP BY admin_level,osm_id;
 insert into tbt_tmp_holes(admin_level,geometry) 
 SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,9))FROM (
-      SELECT admin_level, (ST_Dump(geometry)).geom As geometry
+      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
       FROM osm_new_admin) As foo
    GROUP BY admin_level,osm_id;
 insert into tbt_tmp_holes(admin_level,geometry) 
 SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,10))FROM (
-      SELECT admin_level, (ST_Dump(geometry)).geom As geometry
+      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
       FROM osm_new_admin) As foo
    GROUP BY admin_level,osm_id;
 insert into tbt_tmp_holes(admin_level,geometry) 
 SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,11))FROM (
-      SELECT admin_level, (ST_Dump(geometry)).geom As geometry
+      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
       FROM osm_new_admin) As foo
    GROUP BY admin_level,osm_id;
 
@@ -78,24 +78,28 @@ delete from tbt_tmp_holes where geometry is null;
 
 insert into tbt_tmp_rings (admin_level,geometry) select admin_level,geometry from tbt_tmp_holes;
 
+drop table if exists tom_bnd_2;
+drop table if exists tom_bnd_4;
+drop table if exists tom_bnd_6;
+drop table if exists tom_bnd_8;
 
 create table tom_bnd_2 as (
-   select c1.osm_id, st_intersection(c1.geometry,c2.geometry) as geometry
+   select c1.id, st_intersection(c1.geometry,c2.geometry) as geometry
    from tbt_tmp_rings c1, tbt_tmp_rings c2
-   where c1.geometry && c2.geometry and c1.osm_id!=c2.osm_id and c1.admin_level=2 and c2.admin_level=2
+   where c1.geometry && c2.geometry and c1.id!=c2.id and c1.admin_level=2 and c2.admin_level=2
 );
 create table tom_bnd_4 as (
-   select c1.osm_id, st_intersection(c1.geometry,c2.geometry) as geometry
+   select c1.id, st_intersection(c1.geometry,c2.geometry) as geometry
    from tbt_tmp_rings c1, tbt_tmp_rings c2
-   where c1.geometry && c2.geometry and c1.osm_id!=c2.osm_id and c1.admin_level=4 and c2.admin_level=4
+   where c1.geometry && c2.geometry and c1.id!=c2.id and c1.admin_level=4 and c2.admin_level=4
 );
 create table tom_bnd_6 as (
-   select c1.osm_id, st_intersection(c1.geometry,c2.geometry) as geometry
+   select c1.id, st_intersection(c1.geometry,c2.geometry) as geometry
    from tbt_tmp_rings c1, tbt_tmp_rings c2
-   where c1.geometry && c2.geometry and c1.osm_id!=c2.osm_id and c1.admin_level=6 and c2.admin_level=6
+   where c1.geometry && c2.geometry and c1.id!=c2.id and c1.admin_level=6 and c2.admin_level=6
 );
 create table tom_bnd_8 as (
-   select c1.osm_id, st_intersection(c1.geometry,c2.geometry) as geometry
+   select c1.id, st_intersection(c1.geometry,c2.geometry) as geometry
    from tbt_tmp_rings c1, tbt_tmp_rings c2
-   where c1.geometry && c2.geometry and c1.osm_id!=c2.osm_id and c1.admin_level=8 and c2.admin_level=8
+   where c1.geometry && c2.geometry and c1.id!=c2.id and c1.admin_level=8 and c2.admin_level=8
 );
