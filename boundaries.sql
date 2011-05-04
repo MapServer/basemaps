47b121f347b6bfcd62642f6051f597cecd0024a9
Thomas Bonfort <thomas.bonfort@gmail.com>
2011-05-04 19:21:06 +0000

    more efficient interiorring extraction

--- boundaries.sql
+++ boundaries.sql
@@ -1,5 +1,4 @@
 drop table if exists tbt_tmp_rings;
-drop table if exists tbt_tmp_holes;
 
 
 create table tbt_tmp_rings (id serial, admin_level smallint, geometry geometry);
@@ -11,73 +10,18 @@ insert into tbt_tmp_rings(admin_level,geometry)
       FROM osm_new_admin) As foo
    GROUP BY admin_level,osm_id;
 
+
+insert into tbt_tmp_rings (admin_level,geometry) 
+   select admin_level,st_collect(geom)
+      from
+      (select admin_level, ST_InteriorRingN(geom, generate_series(1, ST_NumInteriorRing(geom))) as geom
+         from 
+         (select admin_level,
+            st_geometryN(geom, generate_series(1, st_numgeometries(geom))) as geom
+            from
+            (select admin_level, geometry as geom from osm_new_admin) as foo) as bar) as baz group by admin_level;
+
 create index tbt_tmp_rings_idx on tbt_tmp_rings using gist(geometry);
-
-
-create table tbt_tmp_holes (id serial, admin_level smallint, geometry geometry);
-
-create index tbt_tmp_holes_idx on tbt_tmp_holes using gist(geometry);
-
-insert into tbt_tmp_holes(admin_level,geometry) 
-SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,1))FROM (
-      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
-      FROM osm_new_admin) As foo
-   GROUP BY admin_level,osm_id;
-insert into tbt_tmp_holes(admin_level,geometry) 
-SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,2))FROM (
-      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
-      FROM osm_new_admin) As foo
-   GROUP BY admin_level,osm_id;
-insert into tbt_tmp_holes(admin_level,geometry) 
-SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,3))FROM (
-      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
-      FROM osm_new_admin) As foo
-   GROUP BY admin_level,osm_id;
-insert into tbt_tmp_holes(admin_level,geometry) 
-SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,4))FROM (
-      SELECT  admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
-      FROM osm_new_admin) As foo
-   GROUP BY admin_level,osm_id;
-insert into tbt_tmp_holes(admin_level,geometry) 
-SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,5))FROM (
-      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
-      FROM osm_new_admin) As foo
-   GROUP BY admin_level,osm_id;
-insert into tbt_tmp_holes(admin_level,geometry) 
-SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,6))FROM (
-      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
-      FROM osm_new_admin) As foo
-   GROUP BY admin_level,osm_id;
-insert into tbt_tmp_holes(admin_level,geometry) 
-SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,7))FROM (
-      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
-      FROM osm_new_admin) As foo
-   GROUP BY admin_level,osm_id;
-insert into tbt_tmp_holes(admin_level,geometry) 
-SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,8))FROM (
-      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
-      FROM osm_new_admin) As foo
-   GROUP BY admin_level,osm_id;
-insert into tbt_tmp_holes(admin_level,geometry) 
-SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,9))FROM (
-      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
-      FROM osm_new_admin) As foo
-   GROUP BY admin_level,osm_id;
-insert into tbt_tmp_holes(admin_level,geometry) 
-SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,10))FROM (
-      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
-      FROM osm_new_admin) As foo
-   GROUP BY admin_level,osm_id;
-insert into tbt_tmp_holes(admin_level,geometry) 
-SELECT admin_level, ST_Collect(ST_InteriorRingN(geometry,11))FROM (
-      SELECT admin_level,osm_id,  (ST_Dump(geometry)).geom As geometry
-      FROM osm_new_admin) As foo
-   GROUP BY admin_level,osm_id;
-
-delete from tbt_tmp_holes where geometry is null;
-
-insert into tbt_tmp_rings (admin_level,geometry) select admin_level,geometry from tbt_tmp_holes;
-
 drop table if exists tom_bnd_2;
 drop table if exists tom_bnd_4;
 drop table if exists tom_bnd_6;
@@ -103,3 +47,5 @@ create table tom_bnd_8 as (
    from tbt_tmp_rings c1, tbt_tmp_rings c2
    where c1.geometry && c2.geometry and c1.id!=c2.id and c1.admin_level=8 and c2.admin_level=8
 );
+
+
