71d07d17fe2f37067d2f7d34c09fe5d85e87022b
Nicolasribot <nicolas.ribot@gmail.com>
2017-01-10 14:56:10 +0100

    no more sliders for transp

--- html-demo/js/leaflet.rasterctrl-src.js
+++ html-demo/js/leaflet.rasterctrl-src.js
@@ -28,7 +28,7 @@ L.Control.RasterCtrl = L.Control.extend({
     "<input type='text' id='rval4' name='rval4' value='350'/> <input type='color' id='clr4' name='clr4' value='#C83737'><br/>" +
     "<input type='text' id='rval5' name='rval5' value='403'/> <input type='color' id='clr5' name='clr5' value='#FF0000'><br/>" +
     "<input type='checkbox' id='slope' name='slope'/> Ombrages relief + alti ramp<br/>" +
-    "</div><div>" +
+    "</div><div style='display: none'>" +
     "93048: 0%<input type='range' value='75' id='clrrangeSlider1' min='0' max='100' step='2'>100%<br/>" +
     "06088: 0%<input type='range' value='75' id='clrrangeSlider2' min='0' max='100' step='2'>100%<br/>" +
     "35051: 0%<input type='range' value='75' id='clrrangeSlider3' min='0' max='100' step='2'>100%<br/>" +
