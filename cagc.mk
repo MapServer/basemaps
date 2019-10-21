939af78fc64f58e6b6b3c1f4b17bb93d69f14cad
Jacolin <yves.jacolin@camptocamp.com>
2019-10-21 16:57:15 +0200

    Delete custom makefile

--- cagc.mk
+++
@@ -1,7 +0,0 @@
-OSM_EXTENT=404276 6546698 645590 6691451
-OSM_SRID=2154
-OSM_UNITS=meters
-OSM_DB_CONNECTION=host=localhost dbname=osm user=www-data password=www-data port=5432
-STYLE=google
-
-include Makefile
